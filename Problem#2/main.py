feb4fee50410245988dae1ae9a68ac43a31b819e
rileyspollard <rileyspollard@outlook.com>
2024-02-14 12:41:38 -0600

    Problem 2 and 3

--- Problem#2/main.py
+++ Problem#2/main.py
@@ -1,9 +1,17 @@
+# Riley Pollard
+# Wednesday class, Thursday 2pm lab
+
 def driving_cost(miles_per_gallon, dollars_per_gallon, miles_driven):
    #write your code here
+   dolpermil = dollars_per_gallon/miles_per_gallon
+   return (dolpermil*miles_driven)
+   
 
 if __name__ == '__main__':
-    miles_per_gallon = float(input())
-    dollars_per_gallon = float(input())
-    #print your costs here like example below
-    #print(f'{driving_cost(miles_per_gallon, dollars_per_gallon, 10.0):.2f}')
+   miles_per_gallon = float(input())
+   dollars_per_gallon = float(input())
+   #print your costs here like example below
+   print(f'{driving_cost(miles_per_gallon, dollars_per_gallon, 10.0):.2f}')
+   print(f'{driving_cost(miles_per_gallon, dollars_per_gallon, 50.0):.2f}')
+   print(f'{driving_cost(miles_per_gallon, dollars_per_gallon, 100.0):.2f}')
    
--- Problem#3/main.py
+++ Problem#3/main.py
@@ -1,8 +1,16 @@
+# Riley Pollard
+# Wednesday class, Thursday 2pm lab
+
 def feet_to_steps(user_feet):
    #write your code here
+   
+   return (int(user_feet /2.5))
 
 if __name__ == '__main__':
     #take input feet steps here
+   feet = float(input())
+   
     #store it into the function
-    
+
     #print your steps here
+   print (int(feet_to_steps(feet)))
