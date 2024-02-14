1429d8639f3a2f375d1caf35d227c3a5b35192ac
rileyspollard <rileyspollard@outlook.com>
2024-02-14 13:33:24 -0600

    Problem 4

--- problem#4/main.py
+++ problem#4/main.py
@@ -1,15 +1,28 @@
+# Riley Pollard
+# Thursday 2pm lab, Wednesday class
+
 def int_to_reverse_binary(num1):
     binary_val = ''
 #write your while loop here
     while num1 > 0:
         #write your code
+        placement = num1 %2
+        binary_val += str(placement)
+        num1 = num1 // 2
+        
 
-    return binary_val;
+
+    return binary_val
 
 
 def string_reverse(input_string): 
     reverse_input = ''
-    
+    length = 0
+    strlen = len(input_string)
+    while length < len(input_string):
+        reverse_input += input_string[strlen-1-length]
+        print(reverse_input)
+        length += 1
    #write your for loop here
     
     return reverse_input
